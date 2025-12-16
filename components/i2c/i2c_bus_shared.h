717eb16c733c24c307e065c112764014a06576b6
novaeco <novatribolnovea@gmail.com>
2025-12-16 10:35:09 +0100

    Stabilize SD ext CS timing and I2C locking

--- components/i2c/i2c_bus_shared.h
+++ components/i2c/i2c_bus_shared.h
@@ -2,6 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include "esp_err.h"
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,6 +18,19 @@ extern SemaphoreHandle_t g_i2c_bus_mutex;
  *        Speed: 400kHz
  */
 void i2c_bus_shared_init(void);
+
+/**
+ * @brief Acquire the shared I2C bus mutex.
+ *
+ * @param timeout_ticks Timeout in FreeRTOS ticks.
+ * @return true on success, false if the lock could not be taken.
+ */
+bool i2c_bus_shared_lock(TickType_t timeout_ticks);
+
+/**
+ * @brief Release the shared I2C bus mutex.
+ */
+void i2c_bus_shared_unlock(void);
 
 #ifdef __cplusplus
 }
